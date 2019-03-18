a45ac65e92e2ef55445abd2a4a0e81618214b09e
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-03-18 23:00:55 +0300

    Refactor Rect class

--- src/Objects/Rect.js
+++ src/Objects/Rect.js
@@ -1,59 +1,59 @@
 import ElementTransformer from '../ElementTransformer';
 import Coordinates from '../DataStructures/Coordinates';
 
-var Rect = new Object();
+const Rect = {
+  translate: function (coordinates) {
+    this.setAttribute('x', Number(this.getAttribute('x')) + coordinates.x);
+    this.setAttribute('y', Number(this.getAttribute('y')) + coordinates.y);
+    const rotate = ElementTransformer.getTransformAttribute(this, 'rotate');
+    if (rotate != undefined) {
+      ElementTransformer.setTransformAttribute(this, 'rotate', rotate[0] + ' ' + (rotate[1] + coordinates.x) + ' ' + (rotate[2] + coordinates.y));
+    }
+    return this;
+  },
 
-Rect.translate = function (x, y) {
-  this.setAttribute('x', Number(this.getAttribute('x')) + x);
-  this.setAttribute('y', Number(this.getAttribute('y')) + y);
-  var rotate = ElementTransformer.getTransformAttribute(this, 'rotate');
-  if (rotate != undefined) {
-    ElementTransformer.setTransformAttribute(this, 'rotate', rotate[0] + ' ' + (rotate[1] + x) + ' ' + (rotate[2] + y));
+  getPosition: function () {
+    return {
+      x: Number(this.getAttribute('x')),
+      y: Number(this.getAttribute('y'))
+    }
+  },
+
+  getSize: function () {
+    return {
+      width: Number(this.getAttribute('width')),
+      height: Number(this.getAttribute('height'))
+    }
+  },
+
+  setPosition: function (x, y) {
+    this.setAttribute('x', x);
+    this.setAttribute('y', y);
+    return this;
+  },
+
+  setSize: function (width, height) {
+    this.setAttribute('width', width);
+    this.setAttribute('height', height);
+    return this;
+  },
+
+  scale: function (width, height) {
+    this.setAttribute('width', Number(this.getAttribute('width')) * width);
+    this.setAttribute('height', Number(this.getAttribute('height')) * height);
+    return this;
+  },
+
+  rotate: function (angle) {
+    const elPos = this.getPosition();
+    const elSize = this.getSize();
+    const centerCoordinates = new Coordinates(
+      elPos.x + elSize.width / 2,
+      elPos.y + elSize.height / 2
+    );
+    ElementTransformer.rotate(this, centerCoordinates, angle);
+    return this;
   }
-  return this;
-}
-
-Rect.getPosition = function () {
-  return {
-    x: Number(this.getAttribute('x')),
-    y: Number(this.getAttribute('y'))
-  }
-}
-
-Rect.getSize = function () {
-  return {
-    width: Number(this.getAttribute('width')),
-    height: Number(this.getAttribute('height'))
-  }
-}
-
-Rect.setPosition = function (x, y) {
-  this.setAttribute('x', x);
-  this.setAttribute('y', y);
-  return this;
-}
-
-Rect.setSize = function (width, height) {
-  this.setAttribute('width', width);
-  this.setAttribute('height', height);
-  return this;
-}
-
-Rect.scale = function (width, height) {
-  this.setAttribute('width', Number(this.getAttribute('width')) * width);
-  this.setAttribute('height', Number(this.getAttribute('height')) * height);
-  return this;
-}
-
-Rect.rotate = function (angle) {
-  var elPos = this.getPosition();
-  var elSize = this.getSize();
-  var centerCoordinates = new Coordinates(
-    elPos.x + elSize.width / 2,
-    elPos.y + elSize.height / 2
-  );
-  ElementTransformer.rotate(this, centerCoordinates, angle);
-  return this;
-}
+};
 
 export default Rect;
