aba3affb829bdf27bc68c05811ec5de643af4c0f
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-05-12 20:41:08 +0300

    Update tests

--- build/tests.js
+++ build/tests.js
@@ -2,78 +2,58 @@ var svgEl = document.createElement('svg');
 svgEl.setAttribute('width', 400);
 svgEl.setAttribute('height', 400);
 var props = {
-	svg: svgEl,
+  svg: svgEl,
 };
 var editor = new SvgEditor.default(props);
 
+function svgObjectTests(object, assert) {
+  editor.add(object);
+
+  assert.deepEqual(object.getPosition(), { x: 20, y: 40 }, 'Correctly position at initialization');
+  assert.deepEqual(object.getSize(), { width: 80, height: 20 }, 'Correctly size at initialization');
+
+  object.translate(10, 20);
+  assert.deepEqual(object.getPosition(), { x: 30, y: 60 }, 'Correctly translates position');
+
+  object.scale(2, 2);
+  assert.deepEqual(object.getSize(), { width: 160, height: 40 }, 'Correctly scales size');
+
+  object.setPosition(40, 80);
+  assert.deepEqual(object.getPosition(), { x: 40, y: 80 }, 'Correctly changes position');
+  object.setSize(100, 40);
+  assert.deepEqual(object.getSize(), { width: 100, height: 40 }, 'Correctly changes size');
+
+  object.rotateTransform(-270);
+  assert.deepEqual(object.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -270 degrees');
+  object.rotateTransform(-180);
+  assert.deepEqual(object.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -180 degrees');
+  object.rotateTransform(-90);
+  assert.deepEqual(object.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -90 degrees');
+}
+
 QUnit.test("Rect transformations", function (assert) {
-	var rect = editor.factory.rect({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
-	editor.add(rect);
-
-	assert.deepEqual(rect.getPosition(), { x: 20, y: 40 }, 'Correctly position at initialization');
-	assert.deepEqual(rect.getSize(), { width: 80, height: 20 }, 'Correctly size at initialization');
-
-	rect.setPosition(40, 80);
-	assert.deepEqual(rect.getPosition(), { x: 40, y: 80 }, 'Correctly changes position');
-	rect.setSize(100, 40);
-	assert.deepEqual(rect.getSize(), { width: 100, height: 40 }, 'Correctly changes size');
-
-	rect.rotateTransform(-270);
-	assert.deepEqual(rect.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -270 degrees');
-	rect.rotateTransform(-180);
-	assert.deepEqual(rect.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -180 degrees');
-	rect.rotateTransform(-90);
-	assert.deepEqual(rect.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -90 degrees');
+  const rect = editor.factory.rect({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
+  svgObjectTests(rect, assert);
 });
 
 QUnit.test("Ellipse transformations", function (assert) {
-	var ellipse = editor.factory.ellipse({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
-	editor.add(ellipse);
-
-	assert.deepEqual(ellipse.getPosition(), { x: 20, y: 40 }, 'Correctly position at initialization');
-	assert.deepEqual(ellipse.getSize(), { width: 80, height: 20 }, 'Correctly size at initialization');
-
-	ellipse.setPosition(40, 80);
-	assert.deepEqual(ellipse.getPosition(), { x: 40, y: 80 }, 'Correctly changes position');
-	ellipse.setSize(100, 40);
-	assert.deepEqual(ellipse.getSize(), { width: 100, height: 40 }, 'Correctly changes size');
-
-	ellipse.rotateTransform(-270);
-	assert.deepEqual(ellipse.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -270 degrees');
-	ellipse.rotateTransform(-180);
-	assert.deepEqual(ellipse.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -180 degrees');
-	ellipse.rotateTransform(-90);
-	assert.deepEqual(ellipse.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -90 degrees');
+  const ellipse = editor.factory.ellipse({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
+  svgObjectTests(ellipse, assert);
 });
 
 QUnit.test("Triangle path transformations", function (assert) {
-	var path = editor.factory.path({ x: 20, y: 40, width: 80, height: 20, d: 'M0,40 L20,0 L40,40 Z', fill: '#0000FF' });
-	editor.add(path);
-
-	assert.deepEqual(path.getPosition(), { x: 20, y: 40 }, 'Correctly position at initialization');
-	assert.deepEqual(path.getSize(), { width: 80, height: 20 }, 'Correctly size at initialization');
-
-	path.setPosition(40, 80);
-	assert.deepEqual(path.getPosition(), { x: 40, y: 80 }, 'Correctly changes position');
-	path.setSize(100, 40);
-	assert.deepEqual(path.getSize(), { width: 100, height: 40 }, 'Correctly changes size');
-
-	path.rotateTransform(-270);
-	assert.deepEqual(path.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -270 degrees');
-	path.rotateTransform(-180);
-	assert.deepEqual(path.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -180 degrees');
-	path.rotateTransform(-90);
-	assert.deepEqual(path.getPosition(), { x: 40, y: 80 }, 'Correct position after rotate by -90 degrees');
+  const path = editor.factory.path({ x: 20, y: 40, width: 80, height: 20, d: 'M0,40 L20,0 L40,40 Z', fill: '#0000FF' });
+  svgObjectTests(path, assert);
 });
 
 QUnit.test("Event hander", function (assert) {
-	var done = assert.async();
-	var onChangeHandler = function () {
-		assert.equal(true, true);
-		done();
-	};
-	editor.onChange = onChangeHandler;
+  var done = assert.async();
+  var onChangeHandler = function () {
+    assert.equal(true, true);
+    done();
+  };
+  editor.onChange = onChangeHandler;
 
-	var rect = editor.factory.rect({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
-	editor.add(rect);
+  var rect = editor.factory.rect({ x: 20, y: 40, width: 80, height: 20, fill: '#0000FF' });
+  editor.add(rect);
 });
