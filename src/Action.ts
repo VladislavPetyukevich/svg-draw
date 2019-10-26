fde267dc14d2cf51d0ae26832b5843d6aa7c06ac
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-10-26 22:44:03 +0300

    addElement default parameters

--- src/Action.ts
+++ src/Action.ts
@@ -8,13 +8,16 @@ export type Action = (state: State, setState: SetState) => Partial<Element>;
 export const addElement = (stateChanger: StateChanger, parameters: Partial<Element>) =>
   stateChanger(
     (state: State, setState: SetState) => {
+      if (!parameters.type) {
+        throw new Error('Element type is not specified');
+      }
       const newElement: Element = {
         id: state.elements.length,
-        type: parameters.type!,
-        x: parameters.x!,
-        y: parameters.y!,
-        width: parameters.width!,
-        height: parameters.height!
+        type: parameters.type,
+        x: parameters.x || 0,
+        y: parameters.y || 0,
+        width: parameters.width || 0,
+        height: parameters.height || 0
       };
       setState({
         elements: [...state.elements, newElement]
